e9f1ec741d3d13bd4781f71162b1a7bd81a961b6
Anshul Khandelwal <anshul@anshul.io>
2020-04-29 18:39:35 +0530

    refactor(v2): cleans up filter date

--- src/utils/filterData.ts
+++ src/utils/filterData.ts
@@ -1,15 +1,14 @@
 export type DateRangeT = 'all_time' | 'last_30_days' | 'last_7_days'
 
 export const filterData = (dateRange: DateRangeT, data: ReadonlyArray<unknown>) => {
-  const dataLength = data.length
   switch (dateRange) {
     case 'all_time':
       return data
     case 'last_30_days':
-      const monthRange = dataLength >= 30 ? 30 : dataLength
-      return data.slice(dataLength - monthRange)
+      if (data.length <= 30) return data
+      return data.slice(data.length - 30)
     case 'last_7_days':
-      const weekRange = dataLength >= 7 ? 7 : dataLength
-      return data.slice(dataLength - weekRange)
+      if (data.length <= 7) return data
+      return data.slice(data.length - 7)
   }
 }
