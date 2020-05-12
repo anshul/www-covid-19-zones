84768bb9e9d51d90fc52f4dae9be5d72175242dc
Anshul Khandelwal <anshul@anshul.io>
2020-05-13 00:50:44 +0530

    feat(v2): add a choropleth

--- src/pages/V2Home/Choropleth.tsx
+++ src/pages/V2Home/Choropleth.tsx
@@ -1,10 +1,14 @@
 import React, { useRef, useEffect } from 'react'
 import { makeStyles, createStyles } from '@material-ui/styles'
+import * as d3 from 'd3'
+import * as topojson from 'topojson'
 
-interface Props {
+export interface ChoroplethResponse {
   data?: any
   error?: string
 }
+
+type Props = ChoroplethResponse
 
 const useStyles = makeStyles(() =>
   createStyles({
@@ -15,6 +19,13 @@ const useStyles = makeStyles(() =>
     },
   })
 )
+declare global {
+  interface Window {
+    choropleth: any
+  }
+}
+
+window.choropleth = window.choropleth || { topojson, d3 }
 
 const Choropleth: React.FC<Props> = ({ data, error }) => {
   const classes = useStyles()
@@ -23,9 +34,24 @@ const Choropleth: React.FC<Props> = ({ data, error }) => {
     const maybeDiv: unknown = mapRef.current
     if (!maybeDiv) return
     const el: HTMLElement = maybeDiv as HTMLElement
+    if (!data) return
+    const map = data.map
+    window.choropleth.map = map
+
+    const svg = d3.select(el).append('svg').attr('height', 500).attr('width', 500)
+    const g = svg.append('g')
+    window.choropleth.g = g
+    const projection = d3.geoMercator()
+    const features = (topojson.feature(map, map.objects.covid19zones) as any).features
+
+    const path: any = d3.geoPath()
+    g.selectAll('path').data(features).enter().append('path').attr('d', path).style('stroke-width', '2')
+
     console.log('d3 update', el, data, error)
+
     return () => {
       console.log('d3 cleanup')
+      d3.select(el).select('svg').remove()
     }
   }, [data, error])
 
--- src/pages/V2Home/V2HomeRoot.tsx
+++ src/pages/V2Home/V2HomeRoot.tsx
@@ -1,6 +1,5 @@
-import React, { useRef, useEffect } from 'react'
+import React from 'react'
 import { graphql } from 'babel-plugin-relay/macro'
-import { makeStyles, createStyles } from '@material-ui/styles'
 import { createFragmentContainer } from 'react-relay'
 import { V2HomeRoot_data } from '../../__generated__/V2HomeRoot_data.graphql'
 import { Grid, Row, Col } from 'react-flexbox-grid'
@@ -10,7 +9,7 @@ import useSWR from 'swr'
 import ErrorPanel from './ErrorPanel'
 import ZoneBar from './ZoneBar'
 import CompareBar from './CompareBar'
-import Choropleth from './Choropleth'
+import Choropleth, { ChoroplethResponse } from './Choropleth'
 
 interface Props {
   data: V2HomeRoot_data | null
@@ -27,22 +26,11 @@ const emptyZone = {
   name: '',
 }
 
-const useStyles = makeStyles(() =>
-  createStyles({
-    mapRoot: {
-      minWidth: '500px',
-      minHeight: '500px',
-      border: '1px solid black',
-    },
-  })
-)
-
 const V2HomeRoot: React.FC<Props> = ({ data, mode, go, dateRange, logScale }) => {
-  const classes = useStyles()
   const onSearch = (code: string) => go({ codes: mode === 'compare' ? [code] : [code] })
   const firstZone = data && data.zones.length >= 1 ? data.zones[0] : null
   const codes = data && data.zones.length >= 1 ? data.zones.map((z) => z.code) : ['in']
-  const response: any = useSWR(`/api/maps?codes=${codes.join(',')}`)
+  const response: ChoroplethResponse = useSWR(`/api/maps?codes=${codes.join(',')}`)
   if (data) {
     if (!MODES.find((m) => m === mode)) return <ErrorPanel onSearch={onSearch} message='Page not found!' />
     if (!firstZone) return <ErrorPanel onSearch={onSearch} message='Invalid zone!' />
