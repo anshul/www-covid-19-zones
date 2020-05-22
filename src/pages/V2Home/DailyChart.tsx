e02b833e4a8fedefe5840c21b507ea091eac10ac
Danish Shah <shah.danish.1996@gmail.com>
2020-05-22 06:40:04 +0530

    feat(daily-chart): add daily chart

--- src/pages/V2Home/DailyChart.tsx
+++ src/pages/V2Home/DailyChart.tsx
@@ -1,10 +1,10 @@
 // @ts-nocheck
-import React, { useEffect, memo } from 'react'
 import { createStyles, makeStyles } from '@material-ui/core'
 import * as d3 from 'd3'
-import { V2HomeRoot_data } from '../../__generated__/V2HomeRoot_data.graphql'
+import React, { memo, useEffect } from 'react'
 import useResponsiveView from '../../hooks/useResponsiveView'
 import { DateRangeT, UrlT } from '../../types'
+import { V2HomeRoot_data } from '../../__generated__/V2HomeRoot_data.graphql'
 
 interface Props {
   colorMap: {
@@ -36,7 +36,7 @@ const useStyles = makeStyles(() =>
 
 const DailyChart: React.FC<Props> = ({ data, go, mode, codes, dateRange, logScale, colorMap }) => {
   const classes = useStyles()
-  const view = useResponsiveView({ marginTop: 5, marginLeft: 50, marginBottom: 30, marginRight: 5 })
+  const view = useResponsiveView({ marginTop: 5, marginLeft: 5, marginBottom: 30, marginRight: 50 })
 
   useEffect(() => {
     console.log('render daily chart', { data })
@@ -63,31 +63,57 @@ const DailyChart: React.FC<Props> = ({ data, go, mode, codes, dateRange, logScal
     const maxValue = d3.max(data.zones.flatMap((zone) => zone.chart.map((point) => point.newInf))) || 100
 
     const x = d3.scaleTime().domain([minDate, maxDate]).range([view.marginLeft, view.innerWidth])
-    const y = d3.scaleLinear().domain([minValue, maxValue]).range([view.innerHeight, view.marginTop])
+    const y = d3.scaleLinear().domain([minValue, maxValue]).nice().range([view.innerHeight, view.marginTop])
+
+    const barWidth =
+      Math.floor((x.range()[1] - x.range()[0]) / data.zones.flatMap((zone) => zone.chart.map((point) => parseDt(point.dt))).length) - 1
 
     const gXAxis = svg.select('.xAxis')
     gXAxis.attr('transform', `translate(0, ${view.height - view.marginBottom})`).call(d3.axisBottom(x).ticks(view.innerWidth / 70))
     const gYAxis = svg.select('.yAxis')
-    gYAxis.attr('transform', `translate(${view.marginLeft},0)`).call(d3.axisLeft(y))
+    gYAxis.attr('transform', `translate(${view.width - view.marginRight},0)`).call(d3.axisRight(y))
 
     const t = d3.transition().duration(1000)
     const gChart = svg.select('.chart')
+    gChart.selectAll('.dot').exit().remove()
+    gChart.selectAll('.line').exit().remove()
     const newInfLine = d3
       .line()
       .x((d) => x(parseDt(d.dt)))
-      .y((d) => y(d.newInf))
-    const updater = (selection) =>
+      .y((d) => y(d.newInfSma5))
+
+    const chartUpdater = (selection) =>
       selection
+        .classed('bar', true)
+        .attr('fill', () => 'coral')
+        .attr('x', (d) => x(parseDt(d.dt)) - barWidth / 2)
+        .attr('y', (d) => y(d.newInf))
+        .attr('width', barWidth)
+        .attr('height', (d) => view.innerHeight - y(d.newInf))
+
+    const lineUpdater = (selection) =>
+      selection
+        .classed('line', true)
         .attr('d', (d) => newInfLine(d.chart))
         .style('fill', 'none')
         .style('stroke', (d) => colorMap[d.code])
-        .style('stroke-width', 2)
+        .style('stroke-width', 4)
+
     gChart
-      .selectAll('path')
+      .selectAll('.bar')
+      .data(data.zones[0].chart)
+      .join(
+        (enter) => enter.append('rect').call(chartUpdater),
+        (update) => update.call(chartUpdater),
+        (exit) => exit.call((exit) => exit.transition(t).attr('opacity', 0).remove())
+      )
+
+    gChart
+      .selectAll('.line')
       .data(data.zones, (d) => d.code)
       .join(
-        (enter) => enter.append('path').call(updater),
-        (update) => update.call(updater),
+        (enter) => enter.append('path').call(lineUpdater),
+        (update) => update.call(lineUpdater),
         (exit) => exit.call((exit) => exit.transition(t).attr('opacity', 0).remove())
       )
 
